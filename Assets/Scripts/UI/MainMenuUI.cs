616914a58f41edfe56ad7e99e329040c438b17f7
Jemiy <kahlaa16.2005@gmail.com>
2026-09-20 10:22:57 +0700

    commit kedua?

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Fitting Room");
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
